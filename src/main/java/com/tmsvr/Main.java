95fcbd78644bafdadf39fc7a1bb9f7b7e7779172
Imre Tömösvári <timester12@gmail.com>
2023-05-20 15:12:17 +0200

    Add more cases to main to test

--- src/main/java/com/tmsvr/Main.java
+++ src/main/java/com/tmsvr/Main.java
@@ -16,19 +16,27 @@ public class Main {
         ds.store("key3", "value3");
         ds.store("key4", "value4");
         ds.store("key5", "value5");
+        ds.store("key1", "value1-modified");
 
         ds.flush();
 
         ds.store("key6", "value6");
         ds.store("key7", "value7");
+        ds.store("key3", "value3-modified");
+        ds.store("key5", "value5-modified");
 
         ds.flush();
 
         ds.store("key8", "value8");
         ds.store("key9", "value9");
+        ds.store("key9", "value9-modified");
 
-        System.out.println(ds.get("key2"));
-        System.out.println(ds.get("key6"));
-        System.out.println(ds.get("key8"));
+        System.out.println("key1 - " + ds.get("key1").orElse("not found"));
+        System.out.println("key2 - " + ds.get("key2").orElse("not found"));
+        System.out.println("key3 - " + ds.get("key3").orElse("not found"));
+        System.out.println("key5 - " + ds.get("key5").orElse("not found"));
+        System.out.println("key6 - " + ds.get("key6").orElse("not found"));
+        System.out.println("key8 - " + ds.get("key8").orElse("not found"));
+        System.out.println("key9 - " + ds.get("key9").orElse("not found"));
     }
 }
